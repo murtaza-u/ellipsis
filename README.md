9b3e5b8b1f56e53a57acdaec08a2262f3b561beb
Murtaza Udaipurwala <murtaza@murtazau.xyz>
2024-02-27 14:41:10 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Authentication & Session Management Service
---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "Account - authentication & session management service";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+  outputs = { self, nixpkgs, ... }@inputs:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      formatter.${system} = pkgs.nixpkgs-fmt;
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          go
+          go-tools
+          gopls
+          sqlc
+          awscli2
+          mycli
+        ];
+      };
+    };
+}
