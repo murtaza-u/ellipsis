e4b234a72888c7ba67fc6a6036671c7e43f5393c
Murtaza Udaipurwala <murtaza@murtazau.xyz>
2024-04-11 09:58:13 +0530

    chore: update dependencies

--- flake.nix
+++ flake.nix
@@ -1,54 +1,59 @@
 {
   description = "Ellipsis - authentication & session management service";
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
-  outputs = { self, nixpkgs, ... }@inputs:
-    let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-      version = "24.03.31";
-    in
-    {
-      formatter.${system} = pkgs.nixpkgs-fmt;
-      packages.${system} = rec {
-        ellipsis = pkgs.buildGoModule {
-          pname = "ellipsis";
-          version = version;
-          src = ./.;
-          vendorHash = "sha256-U3X1LSC3SLsFA87Ox3HexFCLbo2U9MjlQIB+XD8zWVk=";
-          CGO_ENABLED = 1;
-          subPackages = [ "cmd/ellipsis" ];
-        };
-        dockerImage = pkgs.dockerTools.buildImage {
-          name = "murtazau/ellipsis";
-          tag = version;
-          copyToRoot = with pkgs.dockerTools; [
-            caCertificates
-          ];
-          config = {
-            Cmd = [ "${ellipsis}/bin/ellipsis" ];
-            WorkingDir = "/data";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, flake-utils, ... }@inputs:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        version = "24.04.10";
+      in
+      {
+        formatter = pkgs.nixpkgs-fmt;
+        packages = rec {
+          ellipsis = pkgs.buildGoModule {
+            pname = "ellipsis";
+            version = version;
+            src = ./.;
+            vendorHash = "sha256-c4Ti6Tve06tPNF5k8Yhl6fFVkGayijK5xvOs2/mxSes=";
+            CGO_ENABLED = 1;
+            subPackages = [ "cmd/ellipsis" ];
           };
+          dockerImage = pkgs.dockerTools.buildImage {
+            name = "murtazau/ellipsis";
+            tag = version;
+            copyToRoot = with pkgs.dockerTools; [
+              caCertificates
+            ];
+            config = {
+              Cmd = [ "${ellipsis}/bin/ellipsis" ];
+              WorkingDir = "/data";
+            };
+          };
+          default = ellipsis;
         };
-        default = ellipsis;
-      };
-      devShells.${system}.default = pkgs.mkShell {
-        packages = with pkgs; [
-          go
-          go-tools
-          gopls
-          sqlc
-          awscli2
-          mycli
-          air
-          nodejs
-          nodePackages.pnpm
-          nodePackages.vscode-langservers-extracted
-          nodePackages.typescript-language-server
-          tailwindcss-language-server
-          prettierd
-        ];
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            nixd
+            nixpkgs-fmt
+            go
+            go-tools
+            gopls
+            sqlc
+            awscli2
+            mycli
+            air
+            nodejs
+            nodePackages.pnpm
+            nodePackages.vscode-langservers-extracted
+            nodePackages.typescript-language-server
+            tailwindcss-language-server
+            prettierd
+          ];
 
-        ELLIPSIS_CONFIG = "./config.yaml";
-      };
-    };
+          ELLIPSIS_CONFIG = "./config.yaml";
+        };
+      });
 }
--- go.mod
+++ go.mod
@@ -3,9 +3,9 @@ module github.com/murtaza-u/ellipsis
 go 1.21.5
 
 require (
-	github.com/a-h/templ v0.2.648
+	github.com/a-h/templ v0.2.663
 	github.com/alexedwards/argon2id v1.0.0
-	github.com/aws/aws-sdk-go v1.51.11
+	github.com/aws/aws-sdk-go v1.51.18
 	github.com/coreos/go-oidc/v3 v3.10.0
 	github.com/go-sql-driver/mysql v1.8.1
 	github.com/golang-jwt/jwt/v5 v5.2.1
@@ -14,10 +14,10 @@ require (
 	github.com/labstack/echo/v4 v4.11.4
 	github.com/mattn/go-sqlite3 v1.14.22
 	github.com/mileusna/useragent v1.3.4
-	github.com/tursodatabase/libsql-client-go v0.0.0-20240327144008-dd14b387afb3
+	github.com/tursodatabase/libsql-client-go v0.0.0-20240410091947-6fedad9244f7
 	github.com/wagslane/go-password-validator v0.3.0
 	github.com/xeonx/timeago v1.0.0-rc5
-	golang.org/x/oauth2 v0.18.0
+	golang.org/x/oauth2 v0.19.0
 	gopkg.in/yaml.v3 v3.0.1
 )
 
@@ -26,7 +26,6 @@ require (
 	github.com/antlr4-go/antlr/v4 v4.13.0 // indirect
 	github.com/go-jose/go-jose/v4 v4.0.1 // indirect
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
-	github.com/golang/protobuf v1.5.4 // indirect
 	github.com/gorilla/context v1.1.2 // indirect
 	github.com/gorilla/securecookie v1.1.2 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
@@ -36,13 +35,11 @@ require (
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
-	golang.org/x/crypto v0.21.0 // indirect
-	golang.org/x/exp v0.0.0-20240325151524-a685a6edb6d8 // indirect
-	golang.org/x/net v0.22.0 // indirect
-	golang.org/x/sys v0.18.0 // indirect
+	golang.org/x/crypto v0.22.0 // indirect
+	golang.org/x/exp v0.0.0-20240409090435-93d18d7e34b8 // indirect
+	golang.org/x/net v0.24.0 // indirect
+	golang.org/x/sys v0.19.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 	golang.org/x/time v0.5.0 // indirect
-	google.golang.org/appengine v1.6.8 // indirect
-	google.golang.org/protobuf v1.33.0 // indirect
-	nhooyr.io/websocket v1.8.10 // indirect
+	nhooyr.io/websocket v1.8.11 // indirect
 )
